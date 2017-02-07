c8799df9fe09df667d671705f21ec968a66e24d6
Laszlo Tarcsanyi <v-ltarcsanyi@expedia.com>
2017-02-07 17:09:54 +0100

    refactor a bit

--- expense-reporter-arm-bpi/expense_reporter.py
+++ expense-reporter-arm-bpi/expense_reporter.py
@@ -1,5 +1,4 @@
 import datetime
-import json
 
 import flask
 import httplib2
@@ -7,8 +6,9 @@ from apiclient import discovery
 from oauth2client import client
 from oauth2client.service_account import ServiceAccountCredentials
 
-oauth_scopes = ['https://www.googleapis.com/auth/spreadsheets',
-                'https://www.googleapis.com/auth/drive']
+SPREADSHEETID = "1ZIU-a7A-YEfz0oQtMDjhjkFIpqiAqqEtBrNza8RXmqs"
+
+oauth_scopes = ['https://www.googleapis.com/auth/spreadsheets']
 
 credentials = ServiceAccountCredentials.from_json_keyfile_name("client_secret.json", oauth_scopes)
 http_auth = credentials.authorize(httplib2.Http())
@@ -29,14 +29,15 @@ class Cell:
         self.x = Cell.translate_to_letter(x)
         self.y = y
 
+    @staticmethod
     def translate_to_letter(x):
         letters = [chr(i) for i in range(ord('A'), ord('Z'))]
         return x if isinstance(x, str) else letters[x]
 
 
-def find_first_empty_row(sheet_service):
-    sheet = sheet_service.spreadsheets().get(spreadsheetId="1ZIU-a7A-YEfz0oQtMDjhjkFIpqiAqqEtBrNza8RXmqs",
-                                             ranges="A1:C142", includeGridData=True).execute()
+def find_first_empty_row():
+    sheet = get_sheet_service().spreadsheets().get(spreadsheetId=SPREADSHEETID,
+                                                   ranges="A1:C142", includeGridData=True).execute()
     for _index, row in enumerate(sheet['sheets'][0]['data'][0]['rowData']):
         values = row['values']
         if len(values) == 3:
@@ -57,6 +58,23 @@ def formatted_date():
     return datetime.datetime.now().strftime("%d")
 
 
+def get_sheet_service():
+    return discovery.build('sheets', 'v4', http=http_auth)
+
+
+def update_sheet(title, amount, row):
+    update_sheet_body = dict()
+    update_sheet_body['values'] = [[formatted_date(), title + " (Bot)", amount]]
+    _range = Range(Cell(0, row), Cell(2, row))
+    sheet_resp = get_sheet_service().spreadsheets().values().update(
+        spreadsheetId=SPREADSHEETID,
+        range=_range.to_range_str(),
+        valueInputOption='RAW',
+        body=update_sheet_body).execute()
+    sheet_resp['message'] = "{0} stored as '{1}' in the {2} line.".format(amount, title, row + 1)
+    return sheet_resp
+
+
 @app.route("/ping")
 def ping():
     return "pong"
@@ -69,23 +87,12 @@ def index():
 
     if not title or not amount:
         flask.abort(400)
-    sheet_service = discovery.build('sheets', 'v4', http=http_auth)
 
-    first_empty_row = find_first_empty_row(sheet_service=sheet_service)
+    first_empty_row = find_first_empty_row()
 
-    update_sheet_body = dict()
-    update_sheet_body['values'] = [[formatted_date(), title + " (Bot)", amount]]
-    _range = Range(Cell(0, first_empty_row), Cell(2, first_empty_row))
+    sheet_resp = update_sheet(title, amount, first_empty_row)
 
-    sheet_resp = sheet_service.spreadsheets().values().update(
-        spreadsheetId="1ZIU-a7A-YEfz0oQtMDjhjkFIpqiAqqEtBrNza8RXmqs",
-        range=_range.to_range_str(),
-        valueInputOption='RAW',
-        body=update_sheet_body).execute()
-
-    sheet_resp['message'] = "{0} stored as '{1}' in the {2} line.".format(amount, title, first_empty_row + 1)
-
-    return flask.Response(json.dumps(sheet_resp), mimetype="application/json")
+    return flask.Response(sheet_resp['message'], mimetype="text/plain")
 
 
 if __name__ == '__main__':
